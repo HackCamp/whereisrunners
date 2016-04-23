f4901b2c24ccf0b44f2fe3de4b1ccb2ae339343f
Hal Seki <hal@plants-web.jp>
2016-04-23 22:42:52 +0900

    success to load staff position

--- src/items.js
+++ src/items.js
@@ -1,42 +1,5 @@
-var staffs = [
-  "#-リーダー-上野まどか",
-  "#-リーダー-熊谷寿博（迫リコー）",
-  "#-リーダー-沼倉貴記（迫リコー）",
-  "#-リーダー-堀野（ヤフー）",
-  "#-リーダー-佐々木忠二（迫リコー）",
-  "#-リーダー-遠藤 修（迫リコー）",
-  "#-リーダー-北浦洋明（迫リコー）",
-  "#-リーダー-北浦洋明（迫リコー）",
-  "#-リーダー-北浦洋明（迫リコー）",
-  "#-リーダー-佐々木 隆（迫リコー）",
-  "#-リーダー-海老名（ヤフー）",
-  "#-リーダー-武井（ヤフー）",
-  "#-リーダー-調整中",
-  "#-リーダー-須永（＋犬飼）（ヤフー）",
-  "#-リーダー-横井（東北電力）",
-  "#-リーダー-山田賢太郎（ヤフー）",
-  "#-サポート-佐藤真司",
-  "#-サポート-上山達也",
-  "#-エイド統括-田中直史",
-  "#-マネージャー-石田幸央（ヤフー）",
-  "#-マネージャー-山田あきえ（ヤフー）",
-  "#-マネージャー-沼田瑞木（ヤフー）",
-  "#-マネージャー-鈴木哲也（ヤフー）",
-  "#-マネージャー-竹川隆",
-  "#-マネージャー-千田さん"
-]
-var items = [
-  "*荷物1",
-  "*荷物2",
-  "*荷物3",
-  "*荷物4",
-  "*荷物5",
-  "*荷物6",
-  "*荷物7",
-  "*荷物8",
-  "*荷物9",
-  "*荷物10"
-]
+var currentStaffIdx = 0
+var currentItemIdx = 0
 var Staff = React.createClass({
   render: function() {
     return (
@@ -63,17 +26,18 @@ var updateItems = function(data){
   console.log(data)
   item = data[0]
   ReactDOM.render(
-    <Item itemid={item.no} place={item.device} time={item.lastdatetime}/>,
+    <Item itemid={item.no} place={item.device} lastdatetime={item.lastdatetime}/>,
     document.getElementById(item.no)
   )
 }
 var updateStaffs = function(data){
-  console.log(data)
   staff = data[0]
+  console.log(staff)
   ReactDOM.render(
-    <Staff staffid={staff.no} place={staff.device} time={staff.lastdatetime}/>,
+    <Staff staffid={staff.no} place={staff.device} lastdatetime={staff.lastdatetime}/>,
     document.getElementById(staff.no)
   )
+  setTimeout(searchNextStaff, 1000);
 }
 var loadAllItems = function(){
   $.ajax({
@@ -94,20 +58,17 @@ var loadAllItems = function(){
     },
   });
 };
-var ajaxcall = function(url, callback){
+var ajaxcall = function(url, mycallback){
   console.log(url)
   $.ajax({
     url: url,
     dataType: 'jsonp',
-    jsonpCallback: callback,
+    jsonpCallback: mycallback,
     success: function(data){
-      console.log('success!')
-      callback(data)
-      //setTimeout(loadData,30 * 1000); // 30 seconds
+      console.log('success!!')
     },
     error: function(jqXHR, textStatus, errorThrown){
       console.log(textStatus+": "+errorThrown);
-      //setTimeout(loadData(cardid),30 * 1000); // 30 seconds
     },
     beforeSend: function(xhr) {
       var credentials = $.base64.encode("ppc:hackcamp");
@@ -119,19 +80,11 @@ var loadItems = function(){
   $("#items").children().each(function(idx,dom){
     var cardid = $(dom).first().attr('id')
     var url = "http://reg.picard.jp/map.php?where=no&no=" + cardid;
-    ajaxcall(url, 'updateItems')
-  });
-};
-var loadStaffs = function(){
-  $("#staffs").children().each(function(idx,dom){
-    var cardid = $(dom).first().attr('id')
-    var url = "http://reg.picard.jp/map.php?where=no&no=" + cardid;
-    ajaxcall(url, 'updateStaffs')
   });
 };
 var updateCards = function(data){
   $.each(data, function(idx, card){
-    if (card.no.startsWith("#")){
+    if (card.no.startsWith("@")){
       $('#staffs').append('<div id="' + card.no + '" class="card"/>')
       ReactDOM.render(
         <Staff staffid={card.no} place="" lastdatetime=""/>,
@@ -146,8 +99,19 @@ var updateCards = function(data){
       )
     }
   });
-  loadStaffs();
+  setTimeout(searchNextStaff, 1000);
   loadItems();
+}
+var searchNextStaff = function(){
+  console.log("search next pos:" + currentStaffIdx)
+  var count = $('#staffs').children().length
+  var cardid = $('#staffs').children()[currentStaffIdx].id;
+  var url = "http://reg.picard.jp/map.php?where=no&no=" + cardid;
+  currentStaffIdx++;
+  if (currentStaffIdx > count){
+    currentStaffIdx=0;
+  }
+  ajaxcall(url, 'updateStaffs')
 }
 $( document ).ready(function() {
   console.log( "ready!" );
